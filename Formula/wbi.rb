57cd75e8bb12cc0d1be0345105cc81e6248b9657
Niels Freier <freier.niels@bcg.com>
2024-07-31 14:01:45 +0200

    test

---
+++ Formula/wbi.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Wbi < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/stumpyfr/world_bank_indicators_cli"
+  version "0.1.0"
+  sha256 "cb61201ae485f6944300a2b4550906c66530bd69e6ed1e7d48cea183f363704f"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test wbi`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
