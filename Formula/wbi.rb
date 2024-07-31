947bcb708cbc63d1ac64f71dab6f299b2cf8ec6b
Niels Freier <freier.niels@bcg.com>
2024-07-31 14:13:42 +0200

    WIP

--- Formula/wbi.rb
+++ Formula/wbi.rb
@@ -4,30 +4,23 @@
 class Wbi < Formula
   desc ""
   homepage ""
-  url "https://github.com/stumpyfr/world_bank_indicators_cli"
-  version "0.2.0"
-  sha256 "cb61201ae485f6944300a2b4550906c66530bd69e6ed1e7d48cea183f363704f"
   license ""
 
-  # depends_on "cmake" => :build
+  depends_on "git"
+  depends_on "go" => :optional
 
-  def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  on_macos do
+    on_arm do
+      url "https://github.com/stumpyfr/world_bank_indicators_cli/releases/download/v0.2.0/wbi_Darwin_arm64.tar.gz"
+      sha256 "1e7d5b32fc7095f54ffaba703372603608e73f96f2ecc55c08ae327f08ad5f2d"
+
+      def install
+        bin.install "wbi"
+      end
+    end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test wbi`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/wbi -v"
   end
 end
