7d71f849c19b5a907e6d06a5010996aca389ee96
Niels Freier <freier.niels@bcg.com>
2024-08-01 17:19:43 +0200

    add intel version for darwin

--- Formula/wbi.rb
+++ Formula/wbi.rb
@@ -1,15 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Wbi < Formula
-  desc ""
-  homepage ""
-  license ""
+  desc "This is a command line interface (CLI) that allows users to interact with the World Bank Indicators API. The CLI allows users to search for indicators per source and to retrieve data for specific indicators."
+  homepage "https://github.com/stumpyfr/world_bank_indicators_cli"
+  license "MIT"
 
   depends_on "git"
   depends_on "go" => :optional
 
   on_macos do
+    on_intel do
+      url "https://github.com/stumpyfr/world_bank_indicators_cli/releases/download/v0.2.0/wbi_Darwin_x86_64.tar.gz"
+      sha256 "fb498ecc0c2acfb0906e97f1d00b3cbca66565d84f46f555ff623fc1a1e99707"
+
+      def install
+        bin.install "wbi"
+      end
+    end
     on_arm do
       url "https://github.com/stumpyfr/world_bank_indicators_cli/releases/download/v0.2.0/wbi_Darwin_arm64.tar.gz"
       sha256 "1e7d5b32fc7095f54ffaba703372603608e73f96f2ecc55c08ae327f08ad5f2d"
